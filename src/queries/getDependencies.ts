41a500c7634c237422a2fc98b4f7737ddb45af37
kindoflew <lewis@ombulabs.com>
2022-09-29 22:51:38 -0400

    revert logging change (#3)

--- src/queries/getDependencies.ts
+++ src/queries/getDependencies.ts
@@ -1,14 +1,10 @@
 import { asyncExec, execWithLog } from "../cli/exec";
 import { PackageList } from "../types";
 
-export async function getDependencies(logs: boolean): Promise<PackageList> {
-  const list = await npmList(logs);
+export async function getDependencies(): Promise<PackageList> {
+  const list = await execWithLog(
+    'Reading your dependencies',
+    async () => await asyncExec(`npm ls --depth=0 --json`)
+  );
   return JSON.parse(list.stdout).dependencies;
-}
-
-async function npmList(logs: boolean) {
-  const execList = async () => await asyncExec(`npm ls --depth=0 --json`);
-  return logs
-    ? await execWithLog('Reading your dependencies', execList)
-    : await execList();
 }
