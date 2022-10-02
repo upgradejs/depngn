76e14d7c1a4f5eb281d3ade45448c61e89915d2c
Juan Vasquez <juan@ombulabs.com>
2022-10-02 00:22:18 -0500

    Add jest with example test

--- src/cli/usage.ts
+++ src/cli/usage.ts
@@ -1,11 +1,15 @@
 export function createUsage() {
-  console.log('Usage:');
-  console.log('depngn <node-version> [options]');
-  console.log('');
-  console.log('Options:');
-  console.log('-h, --help       output usage information');
-  console.log('-r, --reporter   which reporter for output. options are: terminal (default), json');
-  console.log('');
-  console.log('Example:')
-  console.log('depngn 12.0.0 --reporter=json');
+  const usage = `
+  Usage:
+  depngn <node-version> [options]
+
+  Options:
+  -h, --help      output usage information
+  -r, --reporter  which reporter for output. options are: terminal (default), json
+
+  Example:
+  depngn 12.0.0 --reporter=json
+  `
+
+  console.log(usage);
 }
---
+++ tests/cli/usage.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import { createUsage } from '../../src/cli/usage';
+
+describe('createUsage', () => {
+  it('uses console.log', () => {
+    console.log = jest.fn();
+    createUsage();
+    expect(console.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('returns an usage message', () => {
+    console.log = jest.fn();
+    createUsage();
+
+    const usage = `
+  Usage:
+  depngn <node-version> [options]
+
+  Options:
+  -h, --help      output usage information
+  -r, --reporter  which reporter for output. options are: terminal (default), json
+
+  Example:
+  depngn 12.0.0 --reporter=json
+  `
+    expect(console.log).toHaveBeenCalledWith(usage);
+  });
+});
