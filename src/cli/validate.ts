ae55b0b47e678dc8a3ac12ce8e6ebf230ecdd649
Lewis <lewis@ombulabs.com>
2022-10-06 09:57:42 -0400

    remove redundant undefined checks

--- src/cli/validate.ts
+++ src/cli/validate.ts
@@ -3,22 +3,18 @@ import { green, red } from 'kleur/colors';
 
 const REPORTERS = ['terminal', 'json'];
 
-export function validateArgs(nodeVersion: string, reporter?: string) {
+export function validateArgs(nodeVersion: string, reporter: string) {
   validateNodeVersion(nodeVersion);
   validateReporter(reporter);
 }
 
 function validateNodeVersion(nodeVersion: string) {
-  // defaults to current Node version if not specified
-  if (!nodeVersion) return true;
   if (!validate(nodeVersion)) {
     throw new Error(`Invalid Node version: ${red(nodeVersion)}.`);
   }
 }
 
-function validateReporter(reporter?: string) {
-  // defaults to 'terminal' if not specified
-  if (!reporter) return true;
+function validateReporter(reporter: string) {
   if (!REPORTERS.includes(reporter)) {
     throw new Error(
       `Invalid reporter: ${red(reporter)}. Valid options are: ${green(
