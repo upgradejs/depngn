71bc62ee64130b431147cf1eb0ad685d53af57ba
kindoflew <davanzo.lewis@gmail.com>
2023-02-12 16:37:19 -0500

    add CHANGELOG entry

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,7 @@ import { writeFile as syncWriteFile, access as syncAccess } from 'fs';
 // `fs/promises` is only available from Node v14 onwards
 // so we import the sync versions of `writeFile` and `access` and transform them into 
 // async versions using `promisify` (available from Node v8 onwards)
-// if we ever decided to drop support for Node < v14, we can revert to using `fs/promises`
+// if we ever decided to drop support for Node <v14, we can revert to using `fs/promises`
 //
 // `exec` doesn't have a promise version so we have to promisify it no matter what
 
