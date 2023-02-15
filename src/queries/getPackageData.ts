da136aab2c85082b586252f9d711b093280ed98f
kindoflew <davanzo.lewis@gmail.com>
2023-02-15 11:59:19 -0500

    better whitespace regex, account for AND ranges

--- src/queries/getPackageData.ts
+++ src/queries/getPackageData.ts
@@ -13,15 +13,26 @@ function isCompatible(nodeVersion: string, depRange: string) {
   // if a dependency has `*` for the node version, it's always compatible
   if (['x', '*'].includes(depRange)) return true;
 
-  let compatible;
+  return depRange
+    .split('||')
+    .map((range) => removeWhitespace(range))
+    .some((range) => safeSatisfies(nodeVersion, range));
+}
 
-  const logicalOrRegEx = /\|\|/;
-  const spaceRegEx = /\s/g;
-  if (depRange && logicalOrRegEx.test(depRange)) {
-    const rangeArray = depRange.split('||').map((range) => range.replace(spaceRegEx, ''));
-    compatible = rangeArray.some((range) => satisfies(nodeVersion, range));
-  } else {
-    compatible = satisfies(nodeVersion, depRange.replace(spaceRegEx, ''));
-  }
-  return compatible;
+// accounts for `AND` ranges -- ie, `'>=1.2.9 <2.0.0'`
+function safeSatisfies(nodeVersion: string, range: string) {
+  return range.split(' ').every((r) => satisfies(nodeVersion, r));
+}
+
+// trims leading and trailing whitespace and removes whitespace
+// between the comparator operators and the actual version number
+// ie, ' > = 12.0.0 ' becomes '>=12.0.0'
+//
+// does *not* handle whitespace between numbers/decimals 
+// in actual version number (ie, 1 2. 0 .0) -- the RegEx for
+// it would be silly complex, so we handle that in `safeSatisfies`
+// once we know we have a single version
+function removeWhitespace(range: string) {
+  const extraSpaceRegEx = /((?<=(<|>))(\s+)(?=(=)))|(?<=(<|>|=|\^))(\s+)(?=\d)|((?<=(\d|\.))(\s+)(?=(\d|\.)))/g;
+  return range.trim().replace(extraSpaceRegEx, '');
 }
