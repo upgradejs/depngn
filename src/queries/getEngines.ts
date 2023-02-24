086baefb469395a3204bc997ee6ee99b691e059c
kindoflew <davanzo.lewis@gmail.com>
2023-02-24 12:16:40 -0500

    feature: remove fetching engines data

--- src/queries/getEngines.ts
+++ src/queries/getEngines.ts
@@ -1,4 +1,3 @@
-import { asyncExec } from '../utils';
 import {
   EnginesDataArray,
   Manager,
@@ -9,66 +8,41 @@ import path from 'path';
 
 export async function getEngines(deps: PackageList, manager: Manager) {
   try {
-    let enginesData: EnginesDataArray;
-    if (manager.name === PackageManagerName.Npm) {
-      const pkgLock = await require(path.resolve(
-        process.cwd(),
-        'package-lock.json'
-      ));
-      return Object.keys(deps).map((dep) => {
-        const range: string =
-          pkgLock.packages[`node_modules/${dep}`]?.engines?.node || '';
+    switch (manager.name) {
+      case PackageManagerName.Npm: {
+        const pkgLock = await require(path.resolve(
+          process.cwd(),
+          manager.lockFile
+        ));
+        const lockFileVersion = pkgLock.lockfileVersion;
+        const pathPrefix = lockFileVersion === 2 ? 'node_modules/' : '';
+        return Object.keys(deps).map((dep) => {
+          const range: string =
+            pkgLock.packages[`${pathPrefix}${dep}`]?.engines?.node || '';
+          return {
+            package: dep,
+            range,
+          };
+        });
+      }
+    case PackageManagerName.Yarn: {
+      return await Promise.all(Object.keys(deps).map(async (dep) => {
+        const pkg = await require(
+          path.resolve(process.cwd(), 'node_modules', dep, 'package.json')
+        );
+        const range = pkg?.engines?.node || '';
         return {
           package: dep,
-          range,
-        };
-      });
-    } else if (manager.name === PackageManagerName.Yarn) {
-      enginesData = [] as never;
-    } else {
-      enginesData = [] as never;
+          range
+        }
+      }));
     }
-  } catch (error) {
-    throw error;
-  }
-}
-
-function parseEngines(
-  engines: { stdout: string; stderr: string },
-  manager: Manager
-) {
-  const res = engines.stdout ? JSON.parse(engines.stdout) : {};
-  switch (manager.name) {
-    case PackageManagerName.Npm:
-      return res.node ? res.node : '';
-    case PackageManagerName.Yarn:
-      return res.data?.node ? res.data.node : '';
     default:
       const wrong = manager.name as never;
       throw new Error(
         `This error shouldn't happen, but somehow an invalid package manager made it through checks: ${wrong}.`
       );
-  }
-}
-
-async function fetchEngines(
-  deps: PackageList,
-  manager: Manager
-): Promise<EnginesDataArray> {
-  try {
-    // const pkg = await require(path.resolve(process.cwd(), 'package.json'));
-    const pkgLock = await require(path.resolve(
-      process.cwd(),
-      'package-lock.json'
-    ));
-    return Object.keys(deps).map((dep) => {
-      const range: string =
-        pkgLock.packages[`node_modules/${dep}`]?.engines?.node || '';
-      return {
-        package: dep,
-        range,
-      };
-    });
+    }
   } catch (error) {
     throw error;
   }
