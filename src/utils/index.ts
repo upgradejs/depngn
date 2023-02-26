7673b7440c7e06bb930229d9fdeee4da32522a12
kindoflew <davanzo.lewis@gmail.com>
2023-02-26 17:30:37 -0500

    remove asyncExec

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,11 +7,7 @@ import path from 'path';
 // so we import the sync versions of `writeFile` and `access` and transform them into
 // async versions using `promisify` (available from Node v8 onwards)
 // if we ever decided to drop support for Node <v14, we can revert to using `fs/promises`
-//
-// `exec` doesn't have a promise version so we have to promisify it no matter what
-
 export const access = promisify(syncAccess);
-export const asyncExec = promisify(exec);
 export const writeFile = promisify(syncWriteFile);
 
 export const readJsonFile = async <T>(...filepath: Array<string>): Promise<T> => {
