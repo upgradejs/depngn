59611247fd1fe877dab96aadfbbf1a095d194933
kindoflew <lewis@ombulabs.com>
2023-03-03 18:07:06 -0500

    [bugfix]: removing whitespace from malformed version ranges (#37)
    * replace replaceAll with global regex replace
    
    * better whitespace regex, account for AND ranges
    
    * make regex better
    
    * package-lock version bump
    
    * one last regex improvement
    
    * fix comment
    
    * one more comment fix
    
    * one last filter
    
    * actually probably the last regex version
    
    * implement invalid range handling
    
    * update README

--- src/queries/getPackageData.ts
+++ src/queries/getPackageData.ts
@@ -1,7 +1,7 @@
 import { satisfies } from 'compare-versions';
-import { EnginesData } from '../types';
+import { CompatData, EnginesData } from '../types';
 
-export function getPackageData(dep: EnginesData, version: string) {
+export function getPackageData(dep: EnginesData, version: string): CompatData {
   const range = dep.range ? dep.range : 'n/a';
   const compatible = isCompatible(version, dep.range);
   return { compatible, range };
@@ -13,14 +13,38 @@ function isCompatible(nodeVersion: string, depRange: string) {
   // if a dependency has `*` for the node version, it's always compatible
   if (['x', '*'].includes(depRange)) return true;
 
-  let compatible;
-
-  const logicalOrRegEx = /\|\|/;
-  if (depRange && logicalOrRegEx.test(depRange)) {
-    const rangeArray = depRange.split('||').map((range) => range.replaceAll(' ', ''));
-    compatible = rangeArray.some((range) => satisfies(nodeVersion, range));
-  } else {
-    compatible = satisfies(nodeVersion, depRange.replaceAll(' ', ''));
+  try {
+    return depRange
+    .split('||')
+    .map((range) => removeWhitespace(range))
+    .some((range) => safeSatisfies(nodeVersion, range));
+  } catch (error) {
+    if ((error as Error).message.match(/Invalid argument not valid semver/)) {
+      return 'invalid';
+    }
+    throw error;
   }
-  return compatible;
+}
+
+// accounts for `AND` ranges -- ie, `'>=1.2.9 <2.0.0'`
+function safeSatisfies(nodeVersion: string, range: string) {
+  return (
+    range
+      .split(' ')
+      // filter out any whitespace we may have missed with the RegEx -- ie, `'>4   <8'`
+      .filter((r) => !!r)
+      .every((r) => satisfies(nodeVersion, r))
+  );
+}
+
+// trims leading and trailing whitespace and whitespace
+// between the comparator operators and the actual version number
+// version number. ie, ' > = 12.0.0 ' becomes '>=12.0.0'
+function removeWhitespace(range: string) {
+  const comparatorWhitespace = /((?<=(<|>))(\s+)(?=(=)))/g;
+  const comparatorAndVersionWhiteSpace = /(?<=(<|>|=|\^|~))(\s+)(?=\d)/g;
+  return range
+    .trim()
+    .replace(comparatorWhitespace, '')
+    .replace(comparatorAndVersionWhiteSpace, '');
 }
